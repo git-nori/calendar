f290f2a61fa37a4cbcad17ebf4b20067705ec21f
HironoriSato <maza.hs5360@gmail.com>
2020-03-02 23:27:43 +0900

    [add]gapiの初期ロード処理を実装

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,26 @@
   </div>
 </template>
 
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  name: "app",
+  components: {
+  },
+  computed: {
+    ...mapState("apiModule", ["api"]),
+    ...mapState("authModule", ["authorized"])
+  },
+  created() {
+    this.setApi();
+    this.load();
+  },
+  methods: {
+    ...mapActions("apiModule", ["setApi", "load"])
+  }
+};
+</script>
+
 <style>
 </style>
