b33754416ec1728392f5b90262ef189e00ce9f07
HironoriSato <maza.hs5360@gmail.com>
2020-03-02 23:10:44 +0900

    [mod]routerにHome.viewを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="authentification">
+    <b-container>
+      <h2>VueJS + Google Calendar Example</h2>Authentification
+      <button v-if="!authorized" @click="handleAuthClick">Sign In</button>
+      <button v-if="authorized" @click="handleSignoutClick">Sign Out</button>
+      <hr />
+      <button v-if="authorized" @click="getData">Get Data</button>
+      <div class="item-container" v-if="authorized && items">
+        <pre v-html="items"></pre>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions, mapGetters, mapState } from "vuex";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+    };
+  },
+  computed: {
+    // ...mapState('authModule', {
+    //   authorized: 'authorized'
+    // }),
+    ...mapState('authModule', ['authorized']),
+    ...mapState('calendarModule', ['items']),
+  },
+  methods: {
+    ...mapActions('authModule', ['signin', 'signout']),
+    ...mapActions('calendarModule', ['getData']),
+    handleAuthClick(){
+      this.signin()
+    },
+    handleSignoutClick(){
+      this.signout()
+    },
   }
 };
 </script>
