130ac80578a82a2e630d3d546fbb6d6eba609804
HironoriSato <maza.hs5360@gmail.com>
2020-03-03 22:24:53 +0900

    [mod]headerItem compoのレイアウトを修正

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="mb-3">
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="#">CalendarApp</b-navbar-brand>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand :to="{name:'top'}">CalendarApp</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-spacer></b-spacer>
+      <b-navbar-toggle target="nav-collapse" style="font-size: 1em;"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
-          <b-nav-item v-if="!isLoggedIn" @click="signin()">Sign In</b-nav-item>
-          <b-nav-item v-if="isLoggedIn" @click="signout()">Sign Out</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" :to="{name:'home'}">Home</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-form>
+            <b-button v-if="!isLoggedIn" @click="signin()" class="mt-3 mt-md-0 ml-md-3" variant="outline-secondary" size="sm">Sign In</b-button>
+            <b-button v-if="isLoggedIn" @click="signout()" class="mt-3 mt-md-0 ml-md-3" variant="outline-secondary" size="sm">Sign Out</b-button>
+          </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
