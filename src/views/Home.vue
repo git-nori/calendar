b4ee0065bc814e337ee601b646dbc81b01ebfe07
HironoriSato <maza.hs5360@gmail.com>
2020-03-08 22:49:44 +0900

    [add]Home view　にchart-data-table, chart compoを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-      <select-box></select-box>
+    <select-box></select-box>
+    <b-container fluid class="mt-4">
+      <b-row>
+        <b-col md="6">
+          <chart></chart>
+        </b-col>
+        <b-col md="6 mt-3 pt-3">
+          <chart-data-table></chart-data-table>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import SelectBox from "@/components/SelectBox.vue";
+import Chart from "@/components/Chart.vue";
+import ChartDataTable from "@/components/ChartDataTable.vue";
 
 export default {
   name: "Home",
   components: {
-    SelectBox
-  },
-  methods: {
+    SelectBox,
+    Chart,
+    ChartDataTable
   },
 };
 </script>
